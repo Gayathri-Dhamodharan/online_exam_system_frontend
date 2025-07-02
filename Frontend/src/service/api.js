a23887fc69ba021c70fbb1f8179821644a5fb740
T080 <T080@CIPL-ATC00263.colanonline.local>
2025-07-02 14:48:25 +0530

    code update

--- Frontend/src/service/api.js
+++ Frontend/src/service/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://online-exam-system-backend-kr3u.onrender.com",
   headers: { "Conten-Type": "application/json" },
 });
 
